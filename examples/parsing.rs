5de968e822a79c49beb1f2d4396bbff92264635f
Ty Overby <ty@pre-alpha.com>
2017-02-16 18:42:58 -0800

    add

---
+++ examples/parsing.rs
@@ -0,0 +1,13 @@
+extern crate snoot;
+
+const PROGRAM: &'static str = "
+(hello world
+    (片仮名
+        (العَرَبِيَّة‎‎)))
+";
+
+fn main() {
+    let snoot::ParseResult{roots, diagnostics} = snoot::simple_parse(PROGRAM);
+    assert!(diagnostics.is_empty());
+    println!("{:#?}", roots);
+}
